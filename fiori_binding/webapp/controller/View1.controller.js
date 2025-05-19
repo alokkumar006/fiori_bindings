92bdfff6ea5d97f568358bfb061d56837bd84184
Alok Kumar <alokbodmas2018@gmail.com>
2025-05-19 06:19:24 +0000

    update view

--- fiori_binding/webapp/controller/View1.controller.js
+++ fiori_binding/webapp/controller/View1.controller.js
@@ -1,10 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
         onInit() {
+            var oModel = new JSONModel("/model/data.json");
+            this.getView().setModel(oModel);
         }
     });
 });
